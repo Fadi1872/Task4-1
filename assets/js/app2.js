1af3aef86bf28f3cbe10f9b7e85473b302284df2
Freddy18 <fadinoumih18@gmail.com>
2023-09-11 14:46:06 +0300

    final

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -15,11 +15,13 @@ inputs.forEach(input => {
 let printBox = document.querySelector('#boxType');
 let print = document.querySelector('#print');
 let espan = document.querySelector('#eSpan');
+let courser = document.querySelector('#coursor');
 let detailes = {'fname' : 'fadi', 'lname' : 'noumih', 'number' : '0935741791', 'age' : '21', 'country' : 'Syria'}
 function printClick() {
     printBox.innerHTML = '';
     print.classList.add('progress');
     print.setAttribute('disabled', '');
+    courser.style.display = 'inline';
     if (document.querySelector('#fname').value != '') {
         detailes.fname = document.querySelector('#fname').value;
     }
@@ -39,14 +41,16 @@ function printClick() {
     let i = 0;
 
     let paragraph = `Hi ${detailes.fname} ${detailes.lname}, Now I have your number which is ${detailes.number}, also I know your age ${detailes.age}, 
-    you live in ${detailes.country}`;
+    you live in ${detailes.country}.`;
 
 
     let counter = setInterval(() => {
         printBox.append(paragraph[i]);
         i++;
         if (i === paragraph.length){
-            
+            print.classList.remove('progress');
+            print.removeAttribute('disabled', '');
+            courser.style.display = 'none';
             clearInterval(counter);
         }
     }, 100);
